8158adc32543f00870c351d2cba51c5011d77f12
Lucas <luklg@outlook.com>
2025-04-25 11:10:40 -0300

    Criada a funcionalidade de atualizar dados por ID

---
+++ src/main/java/lukgoes/AgregadorInvestimentos/controller/UpdateUserDto.java
@@ -0,0 +1,4 @@
+package lukgoes.AgregadorInvestimentos.controller;
+
+public record UpdateUserDto(String username, String password) {
+}
--- src/main/java/lukgoes/AgregadorInvestimentos/service/UserService.java
+++ src/main/java/lukgoes/AgregadorInvestimentos/service/UserService.java
@@ -1,6 +1,7 @@
 package lukgoes.AgregadorInvestimentos.service;
 
 import lukgoes.AgregadorInvestimentos.controller.CreateUserDto;
+import lukgoes.AgregadorInvestimentos.controller.UpdateUserDto;
 import lukgoes.AgregadorInvestimentos.entity.User;
 import lukgoes.AgregadorInvestimentos.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -40,6 +41,29 @@ public class UserService {
     public List<User> listUsers(){
         return userRepository.findAll();
     }
+
+    public void updateUserById(String userId, UpdateUserDto updateUserDto){
+
+        var id = UUID.fromString(userId);
+        var userEntity = userRepository.findById(id);
+
+        if (userEntity.isPresent()){
+            var user = userEntity.get();
+
+            if (updateUserDto.username() != null){
+                    user.setUsername(updateUserDto.username());
+            }
+
+            if (updateUserDto.password() != null){
+                user.setPassword(updateUserDto.password());
+            }
+
+            userRepository.save(user);
+        }
+
+    }
+
+
 
     public void deleteById(String userId){
         var id = UUID.fromString(userId);
